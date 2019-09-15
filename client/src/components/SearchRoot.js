db92e0bf2b21282611e0a646ebbd411f308fed47
sarah-wu <sarahawu@mit.edu>
2019-09-14 21:26:25 -0400

    icons console log

--- client/src/components/SearchRoot.js
+++ client/src/components/SearchRoot.js
@@ -1,7 +1,31 @@
 import React from "react";
+import Icon from '@material-ui/core/Icon';
 import GoogleMapReact from 'google-map-react';
 import SearcherMarker from './SearcherMarker.js'
 import {getLocation} from "../utils.js";
+
+const styles = {
+  icon : {
+    fontSize: 100,
+    //margin: 40,
+    color: 'white',
+  },
+  iconContainer: {
+    position: 'absolute',
+    bottom: 80,
+    left: 80,
+    right: 80,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  button: {
+    height: 160,
+    width: 160,
+    margin: 20,
+    borderRadius: 320, //twice height/width
+    backgroundColor:'rgb(195, 125, 198)', //TODO: change to theme color
+  }
+}
 
 class SearchRoot extends React.Component {
   constructor(props) {
@@ -21,13 +45,20 @@ class SearchRoot extends React.Component {
     console.log(getLocation())
   }
 
-  render() {
-    return (
-      <div>
-        Searcher Root!
-      </div>
-    )
-    ;
+  smsClicked() {
+    console.log('view messages')
+  }
+
+  infoClicked() {
+    console.log('show info page')
+  }
+
+  clueClicked() {
+    console.log('show clue popup')
+  }
+
+  contactClicked() {
+    console.log('contact manager')
   }
 
 
@@ -44,10 +75,28 @@ class SearchRoot extends React.Component {
             lat={42.3583566}
             lng={-71.101722}
           />
+
         </GoogleMapReact>
+
+        <div style={styles.iconContainer}>
+          <button style={styles.button} onClick={() => this.smsClicked()}>
+            <Icon style={styles.icon}> sms </Icon>
+          </button>
+          <button style={styles.button} onClick={() => this.infoClicked()}>
+            <Icon style={styles.icon}> info </Icon>
+          </button>
+          <button style={styles.button} onClick={() => this.clueClicked()}>
+            <Icon style={styles.icon}> photo_size_select_actual </Icon>
+          </button>
+          <button style={styles.button} onClick={() => this.contactClicked()}>
+            <Icon style={styles.icon}> phone </Icon>
+          </button>
+        </div>
+
       </div>
     );
   }
 }
+
 
 export default SearchRoot;
